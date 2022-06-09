87880c2400bf5f1a7c81f90665037a0bb43208cd
Alina Visan <alycry.alina@gmail.com>
2022-06-09 12:25:29 +0300

    cleanup

--- packages/lab/src/contact-details/ContactMetadataItem.tsx
+++ packages/lab/src/contact-details/ContactMetadataItem.tsx
@@ -1,6 +1,7 @@
 import { ComponentType, forwardRef, HTMLAttributes } from "react";
 import { makePrefixer, IconProps } from "@jpmorganchase/uitk-core";
 import { Div } from "../typography";
+import { ValueComponentProps } from "./internal";
 
 const withBaseName = makePrefixer("uitkContactMetadataItem");
 
@@ -9,6 +10,7 @@ export interface ContactMetadataItemProps
   label?: string;
   value: string;
   icon?: ComponentType<IconProps>;
+  ValueComponent?: ComponentType<ValueComponentProps>;
 }
 
 export const ContactMetadataItem = forwardRef<
